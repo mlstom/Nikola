3b003d3607988eed0108976c2c16cb978d113c67
Milos Tomic <milostomic502@gmail.com>
2025-03-19 09:39:04 +0100

    promena backenda

--- backend/server.js
+++ backend/server.js
@@ -8,14 +8,9 @@ const db = require("./config/db");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = [
-    'http://admin.srv758372.hstgr.cloud',
-    'http://alatinidza.rs'
-];
-
 app.use(cors({
-    origin: function (origin, callback) {
-        callback(null, true); // Dozvoljava sve origin-e
+    origin: (origin, callback) => {
+        callback(null, origin || '*'); // Dozvoli bilo koji origin
     },
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true
@@ -24,7 +19,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/uploads", express.static("./uploads"));
+app.use("/uploads", express.static( "./uploads"));
 
 app.use("/api/proizvod", require("./routes/proizvodRoutes"));
 app.use("/api/proizvodSlika", require("./routes/proizvodSlikaRoutes"));
@@ -36,4 +31,4 @@ app.use("/api/uploads", require("./routes/upload"));
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-});
+  });
