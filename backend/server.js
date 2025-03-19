e364a0fb8f4dc5043bc61885ced82329347d9428
Milos Tomic <milostomic502@gmail.com>
2025-03-19 09:57:24 +0100

    promena backenda

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,6 @@ const db = require("./config/db");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const cors = require("cors");
-
-const cors = require("cors");
-
 const allowedOrigins = [
     "http://admin.srv758372.hstgr.cloud",
     "http://alatinidza.rs" // Dodaj drugi dozvoljeni sajt
