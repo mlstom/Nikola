e4aae3a35e049653556eef8c223a01653d3cfd9a
Milos Tomic <milostomic502@gmail.com>
2025-03-19 09:33:44 +0100

    premena kredencija u backendu

--- backend/server.js
+++ backend/server.js
@@ -8,15 +8,26 @@ const db = require("./config/db");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const allowedOrigins = [
+    'http://admin.srv758372.hstgr.cloud',
+    'http://alatinidza.rs'
+];
+
 app.use(cors({
-    origin: ['*'], // Dozvoli admin panel
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/uploads", express.static( "./uploads"));
+app.use("/uploads", express.static("./uploads"));
 
 app.use("/api/proizvod", require("./routes/proizvodRoutes"));
 app.use("/api/proizvodSlika", require("./routes/proizvodSlikaRoutes"));
@@ -28,4 +39,4 @@ app.use("/api/uploads", require("./routes/upload"));
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-  });
+});
