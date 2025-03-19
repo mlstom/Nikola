effaa415c94cba0d6f3dd1cb8eaf0b75a6ab09ad
Milos Tomic <milostomic502@gmail.com>
2025-03-19 17:33:44 +0100

    promena glavnog url za backend na obe straniece

--- admin/src/context/StateContext.jsx
+++ admin/src/context/StateContext.jsx
@@ -20,7 +20,7 @@ export const StateContext = ({ children }) => {
   const [trIdProizvoda, settrIdProizvoda] = useState()
   const [filteredProizvodi, setFilteredProizvodi] = useState(proizvodi);
   const [searchQuery, setsearchQuery] = useState()
-  const backURL = 'http://147.93.122.78:5000'
+  const backURL = 'https://backend.srv758372.hstgr.cloud'
 
 
   async function fetchProizvodi() {
