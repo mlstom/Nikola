8d60c896529f8d7f70717564a665aa13fd21b97b
Milos Tomic <milostomic502@gmail.com>
2025-03-22 14:21:01 +0100

    proba

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,7 @@ const db = require("./config/db");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-    origins:['*'],
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
