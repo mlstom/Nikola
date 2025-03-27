f0e665bee6d3d984ab6b02cbd91e0f007af751e9
Milos Tomic <milostomic502@gmail.com>
2025-03-27 09:58:09 +0100

    dodavanje build foldera

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,17 @@ import Footer from './components/Footer';
 import { ToastContainer } from 'react-toastify';
 import { useEffect } from 'react';
 import { useStateContext } from './context/StateContext';
+import { useLocation } from 'react-router-dom';
+
 const App = () => {
+  const { pathname } = useLocation();
   const{fetchProizvodi} = useStateContext()
   useEffect(() => {
     fetchProizvodi()
   }, [])
-  
+  useEffect(() => {
+    window.scrollTo(0, 0); 
+  }, [pathname]);
   return (
     <>
       <Header />
