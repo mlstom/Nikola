41504f3ca3ee4fd6dc935675f202582ccf337aed
Milos Tomic <milostomic502@gmail.com>
2025-03-28 21:57:20 +0100

    dodavanje kategorija

--- frontend/src/pages/Proizvodi.jsx
+++ frontend/src/pages/Proizvodi.jsx
@@ -11,7 +11,7 @@ const Proizvodi = () => {
   const [priceRange, setPriceRange] = useState({ min: "", max: "" })
   const kategorije = [
     { id: "Alati", naziv: "Alati", podKategorija: ["Dizalice","Kompresori","Aparati za zavarivanje",'Aku alati', "Agregati",'Električni alati', 'Ručni alati', 'Setovi gedora', 'Specijalni setovi'] },
-    { id: "Dvorište i bašta", naziv: "Dvorište i bašta", podKategorija: ["Trimeri","Motorne testere",'Oprema za trimere', 'Motorni bušači rupa', 'Creva i brze spojke', 'Makaze za orezivanje', 'Ostali baštenski alati'] },
+    { id: "Dvorište i bašta", naziv: "Dvorište i bašta", podKategorija: ["Trimeri","Motorne testere","Pumpe za navodnjavanje",'Oprema za trimere', 'Motorni bušači rupa', 'Creva i brze spojke', 'Makaze za orezivanje', 'Ostali baštenski alati'] },
     { id: "Auto oprema", naziv: "Auto oprema", podKategorija: ['Ambijentalna LED svetla za vozila', 'Audio oprema', 'Halogene auto sijalice', 'Kompresori za automobile i setovi za krpljenje guma', 'LED auto sijalice', 'Obloge volana', ' Presvlake za vozila', 'Punjači akumulatora'] },
   ];
 
