8c1c446767c582546a9f283fbfc2248094f7822b
Milos Tomic <milostomic502@gmail.com>
2025-03-29 12:07:31 +0100

    nesto sam sjebap

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const axios = require("axios");
-const { SitemapStream, streamToPromise } = require("sitemap");
+
 
 const db = require("./config/db");
 
