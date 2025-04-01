c24db39bd6343051c7162a504acc0af29cb88f2d
Milos Tomic <milostomic502@gmail.com>
2025-04-01 22:06:57 +0200

    blaga promena servera

--- backend/server.js
+++ backend/server.js
@@ -23,8 +23,8 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']  // ⚡ Dozvoljava custom header-e
 }));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));  
+app.use(express.urlencoded({ extended: true, limit: '200mb' }));
 
 
 app.use("/uploads", express.static("./uploads"));
