683e8f6bb9710522d1c408740fee1b4f538728ba
Milos Tomic <milostomic502@gmail.com>
2025-04-23 21:54:04 +0200

    google analitka

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,10 +11,10 @@ import App from './App.jsx';
 import { StateContext } from './context/StateContext.jsx';
 import Proizvod from './pages/Proizvod.jsx';
 import Korpa from './pages/Korpa.jsx';
-import ReactGA from 'react-ga'
-const Tracking_Id = "G-4P99H2Y12N";
-ReactGa.initialize(Tracking_Id)
 
+import ReactGA from 'react-ga'
+const Tracking_Id = 'G-4P99H2Y12N';
+ReactGA.initialize(Tracking_Id)
 createRoot(document.getElementById('root')).render(
   <StateContext>
     <BrowserRouter>
