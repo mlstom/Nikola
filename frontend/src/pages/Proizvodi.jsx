4bed3c3dc9b3cde5b9e52129a72faf3ddc36b655
mlstom <93375537+mlstom@users.noreply.github.com>
2025-06-19 20:05:33 +0200

    Update Proizvodi.jsx

--- frontend/src/pages/Proizvodi.jsx
+++ frontend/src/pages/Proizvodi.jsx
@@ -10,11 +10,11 @@ const Proizvodi = () => {
   const{selectedCategories,setSelectedPodCategories,setSelectedCategories,selectedPodCategories} = useStateContext()
   const [priceRange, setPriceRange] = useState({ min: "", max: "" })
   const kategorije = [
-    { id: "Alati", naziv: "Alati", podKategorija: ["Dizalice","Kompresori","Aparati za zavarivanje",'Aku alati', "Agregati",'Električni alati', 'Ručni alati', 'Setovi gedora', 'Specijalni setovi'] },
-    { id: "Dvorište i bašta", naziv: "Dvorište i bašta", podKategorija: ["Trimeri","Motorne testere","Pumpe za navodnjavanje",'Oprema za trimere', 'Motorni bušači rupa', 'Creva i brze spojke', 'Makaze za orezivanje', 'Ostali baštenski alati'] },
-    { id: "Auto oprema", naziv: "Auto oprema", podKategorija: ['Ambijentalna LED svetla za vozila', 'Audio oprema', 'Halogene auto sijalice', 'Kompresori za automobile i setovi za krpljenje guma', 'LED auto sijalice', 'Obloge volana', ' Presvlake za vozila', 'Punjači akumulatora'] },
-  ];
-
+        { id: "Alati", naziv: "Alati", podKategorija: ["Kompresori","Dizalice","Aparati za zavarivanje",'Aku alati', "Agregati",'Električni alati', 'Ručni alati', 'Setovi gedora', 'Specijalni setovi'] },
+        { id: "Dvorište i bašta", naziv: "Dvorište i bašta", podKategorija: ["Trimeri","Pumpe za navodnjavanje","Motorne testere",'Oprema za trimere', 'Motorni bušači rupa', 'Creva i brze spojke', 'Makaze za orezivanje', 'Ostali baštenski alati'] },
+        { id: "Auto oprema", naziv: "Auto oprema", podKategorija: ['Ambijentalna LED svetla za vozila', 'Audio oprema', 'Halogene auto sijalice', 'Kompresori za automobile i setovi za krpljenje guma', 'LED auto sijalice', 'Obloge volana', ' Presvlake za vozila', 'Punjači akumulatora'] },
+        {id:'Kucni aparati', naziv:'Kucni aparati', podKategorija:['Ventilatori']}
+      ];
 
   const sortirajProizvode = (proizvodi) => {
     if (selectedSort === "priceAsc") {
