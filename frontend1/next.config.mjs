dd2855cc3b075d7da887be3236c5b9450db4b0c0
Milos Tomic <milostomic502@gmail.com>
2025-07-19 23:16:08 +0200

    govornje xml gde je sajt

--- frontend1/next.config.mjs
+++ frontend1/next.config.mjs
@@ -1,36 +1,20 @@
 /** @type {import('next-sitemap').IConfig} */
 const nextConfig = {
   images: {
-    domains: ['alatinidza.rs','www.alatinidza.rs'],          // dodeli svoj domen
-    // ili, za App Router i Next.js 13+, možeš koristiti remotePatterns:
-    // remotePatterns: [
-    //   {
-    //     protocol: 'https',
-    //     hostname: 'alatinidza.rs',
-    //     port: '',
-    //     pathname: '/**',
-    //   },
-    // ],
+    domains: ['alatinidza.rs', 'www.alatinidza.rs'],
   },
   reactStrictMode: false,
+
   async rewrites() {
     return [
-      {
-        source: '/',       
-        destination: '/home' 
-      }
-    ]
+      { source: '/',            destination: '/home' },
+      { source: '/placanje',     destination: '/home/placanje' },
+      { source: '/korpa',        destination: '/home/korpa' },
+      { source: '/proizvodi',    destination: '/home/proizvodi' },
+      { source: '/proizvod/:id', destination: '/home/proizvod/:id' },
+    ];
   },
-  async redirects() {
-    return [
-      {
-        source: '/home',    
-        destination: '/',   
-        permanent: true     
-      },
-    ]
-  },
-  
+
 };
 
-export default nextConfig;
+module.exports = nextConfig;
