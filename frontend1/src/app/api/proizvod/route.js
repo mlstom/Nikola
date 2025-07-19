b023025d393b4b1c3af55ebeb88e8eed4ffabd86
Milos Tomic <milostomic502@gmail.com>
2025-07-19 05:28:39 +0200

    o boze

--- frontend1/src/app/api/proizvod/route.js
+++ frontend1/src/app/api/proizvod/route.js
@@ -1,9 +1,6 @@
 import { NextResponse } from "next/server";
 import db from '@/lib/db'; 
 
-import { NextResponse } from 'next/server';
-import db from '@/lib/db';
-
 export async function GET(request) {
   const [rows] = await db.execute(`
     SELECT 
