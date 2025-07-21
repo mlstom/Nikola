d69a89640b79b4f5d895001e356ea95c0b097a53
Milos Tomic <milostomic502@gmail.com>
2025-07-21 21:25:11 +0200

    sta?

--- frontend1/next-sitemap.config.js
+++ frontend1/next-sitemap.config.js
@@ -1,8 +1,23 @@
 /** @type {import('next-sitemap').IConfig} */
+const fetch = require('node-fetch');
+
 module.exports = {
   siteUrl: 'https://alatinidza.rs',
   generateRobotsTxt: true,
   changefreq: 'weekly',
   priority: 0.8,
   exclude: [],
+
+  // Ovo dodaje proizvode dinamički u sitemap
+  additionalPaths: async (config) => {
+    const res = await fetch('https://alatinidza.rs/api/proizvodi');
+    const proizvodi = await res.json();
+
+    return proizvodi.map((proizvod) => ({
+      loc: `/proizvod/${proizvod.id}`,
+      changefreq: 'weekly',
+      priority: 0.8,
+      lastmod: new Date().toISOString(),
+    }));
+  },
 };
--- frontend1/src/app/layout.js
+++ frontend1/src/app/layout.js
@@ -72,7 +72,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="sr">
       <head>
-        <link rel='icon' href="/logo.ico" />
+        <link rel='icon' href="/favicon.ico" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
