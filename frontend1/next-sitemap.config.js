dfb93936d5dbcb646a39a7d9e81cad6b35999495
Milos Tomic <milostomic502@gmail.com>
2025-07-21 21:17:02 +0200

    sta?

--- frontend1/next-sitemap.config.js
+++ frontend1/next-sitemap.config.js
@@ -2,11 +2,7 @@
 module.exports = {
   siteUrl: 'https://alatinidza.rs',
   generateRobotsTxt: true,
-  changefreq: 'daily',
-  priority: 0.7,
-  sitemapSize: 5000,
-  // Ako ti treba custom ruta:
-  // additionalPaths: async (config) => [
-  //   await config.transform(config, '/custom-page'),
-  // ],
+  changefreq: 'weekly',
+  priority: 0.8,
+  exclude: [],
 };
