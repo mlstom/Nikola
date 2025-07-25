304c3fd8ba24c801652a5ed86504538af5c58007
Milos Tomic <milostomic502@gmail.com>
2025-07-25 20:57:46 +0200

    ajde

--- frontend1/src/app/api/upload/route.js
+++ frontend1/src/app/api/upload/route.js
@@ -5,11 +5,12 @@ import path from 'path';
 
 export const config = {
   api: { bodyParser: false },
+  maxSize: 200 * 1024 * 1024, // 100MB (samo kao hint, glavni limit postavlja nginx)
 };
 
 export async function GET() {
   try {
-    const dirPath = path.join(process.cwd(), 'uploads');
+    const dirPath = path.join(process.cwd(), 'public', 'uploads');
     const files = fs.readdirSync(dirPath);
 
     const fileUrls = files.map((fileName) => ({
@@ -25,28 +26,23 @@ export async function GET() {
 
 export async function POST(request) {
   const formData = await request.formData();
-  const file = formData.get('image'); // Očekujemo da je "image" ime polja za upload
+  const file = formData.get('image') ;
 
   if (!file) {
     return NextResponse.json({ error: 'No file uploaded' }, { status: 400 });
   }
 
- const uploadDir = path.join(process.cwd(), 'public', 'uploads');
+  const uploadDir = path.join(process.cwd(), 'public', 'uploads');
   if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir, { recursive: true });
   }
 
-  // Dobijamo ime fajla (može biti undefined)
-  const filename = `${Date.now()}`+file.name || `upload-${Date.now()}`;
-
-  // Path gde ćemo da snimimo fajl
+  const filename = `${Date.now()}-${file.name}`;
   const filePath = path.join(uploadDir, filename);
 
-  // file je Web API File/Blob - možemo da pročita sadržaj sa arrayBuffer()
   const arrayBuffer = await file.arrayBuffer();
   const buffer = Buffer.from(arrayBuffer);
 
-  // Upisujemo fajl u sistem
   fs.writeFileSync(filePath, buffer);
 
   return NextResponse.json({ path: `uploads/${filename}` });
